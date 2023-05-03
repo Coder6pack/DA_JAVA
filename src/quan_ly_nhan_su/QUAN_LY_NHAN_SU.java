871e826a45eaa5ad78fa82136731a7f765e0918e
@nhondeptrai <>
2023-05-03 16:23:13 +0700

    1st commit

---
+++ src/quan_ly_nhan_su/QUAN_LY_NHAN_SU.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package quan_ly_nhan_su;
+
+/**
+ *
+ * @author nhon
+ */
+public class QUAN_LY_NHAN_SU {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+
+    }
+
+}
